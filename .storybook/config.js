63a0a24e0b3fefa6a3dfcc4adc17eb5f6019160d
Alexandru Ghiura <ghalex@gmail.com>
2018-04-07 00:09:55 +0300

    Update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react';
-import { ThemeProvider } from 'styled-components'
-import * as themes from '@coderbox/themes'
 
 const req = require.context('../packages', true, /stories\.js$/)
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
-
-addDecorator((story) => (
-  <ThemeProvider theme={themes.main}>
-    {story()}
-  </ThemeProvider>
-))
 
 configure(loadStories, module);
--- packages/coderbox-forms/src/Input/index.stories.js
+++ packages/coderbox-forms/src/Input/index.stories.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withDocs } from 'storybook-readme'
-
+import { withTheme } from '@coderbox/hocs'
+import * as themes from '@coderbox/themes'
 import * as docs from './docs'
 import { Box, Text } from '@coderbox/atoms'
 import { Input } from '../index'
 
 const stories = storiesOf('forms/Input', module)
 
-stories.add('simple usage', withDocs(docs.main, () => {
+stories.add('simple usage', withTheme(themes.main, withDocs(docs.main, () => {
   return (
     <div>
       <Box color='primary' inverted>
@@ -18,4 +19,4 @@ stories.add('simple usage', withDocs(docs.main, () => {
       </Box>
     </div>
   )
-}))
+})))
